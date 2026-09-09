51f766df6c9bbde8a37da29ecbed543ddade6dfb
hilalaha1233203-tech <hilalaha1233203@gmail.com>
2026-09-09 15:29:44 +0530

    Fix user status handler for Supabase database

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -2,19 +2,18 @@
 
 import datetime
 from configs import Config
-from handlers.database import Database
-
-db = Database(Config.DATABASE_URL, Config.BOT_USERNAME)
+from handlers.database import db
 
 
 async def handle_user_status(bot, cmd):
     chat_id = cmd.from_user.id
     if not await db.is_user_exist(chat_id):
         await db.add_user(chat_id)
-        await bot.send_message(
-            Config.LOG_CHANNEL,
-            f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
-        )
+        if Config.LOG_CHANNEL is not None:
+            await bot.send_message(
+                int(Config.LOG_CHANNEL),
+                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
+            )
 
     ban_status = await db.get_ban_status(chat_id)
     if ban_status["is_banned"]:
