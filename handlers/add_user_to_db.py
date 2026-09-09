bc5a67d2fab36567198cc489aa2edb7d8f5efef7
hilalaha1233203-tech <hilalaha1233203@gmail.com>
2026-09-09 15:48:07 +0530

    Prevent optional log-channel peer errors from breaking the bot

--- handlers/add_user_to_db.py
+++ handlers/add_user_to_db.py
@@ -4,13 +4,17 @@ from configs import Config
 from handlers.database import db
 from pyrogram import Client
 from pyrogram.types import Message
+from pyrogram.errors import PeerIdInvalid
 
 
 async def add_user_to_database(bot: Client, cmd: Message):
     if not await db.is_user_exist(cmd.from_user.id):
         await db.add_user(cmd.from_user.id)
         if Config.LOG_CHANNEL is not None:
-            await bot.send_message(
-                int(Config.LOG_CHANNEL),
-                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
-            )
+            try:
+                await bot.send_message(
+                    int(Config.LOG_CHANNEL),
+                    f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
+                )
+            except PeerIdInvalid:
+                pass
